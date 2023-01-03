d47698d6139b9060e21e3c1d6ea6734cc06bddec
sirayuri <118947615+sirayuri@users.noreply.github.com>
2023-01-03 23:19:59 +0900

    Add files via upload

---
+++ 年越カウントダウン/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace 年越カウントダウン
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(23, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "2024年まであと...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 82);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "日表示";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(14, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 82);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "分表示";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 82);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "秒表示";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(235, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(425, 60);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "remaining time...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(280, 377);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 30);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Times...";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 年越カウントダウン/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 年越カウントダウン
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = Application.ProductName;
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime d = DateTime.Now;
+            label3.Text = string.Format($"現在時刻  month:{d.Month} day:{d.Day} {d.Hour}:{d.Minute}:{d.Second}");
+            int mon = d.Month;
+            if (mon == 01)
+            {
+                mon = 13;
+            }
+            else if (mon == 02)
+            {
+                mon = 14;
+            }
+            double yuriusu1 = Math.Floor(365.25 * (d.Year)) + Math.Floor(d.Year / 400.0) - Math.Floor(d.Year / 100.0) + Math.Floor(30.59 * (mon - 2) + d.Day) - 678912;
+
+            double yuriusu2 = Math.Floor(365.25 * 2024) + Math.Floor(2024 / 400.0) - Math.Floor(2024 / 100.0) + Math.Floor(30.59 * (13 - 2) )+1 - 678912;
+
+            var nokori = yuriusu2 - yuriusu1-1;
+            var nokori_minutes = (nokori * 1440)+((24-d.Hour)*60)+(60-d.Minute);
+            var nokori_second = (nokori_minutes * 60) + (60 - d.Second);
+            if (i == 1)
+            {
+                label2.Text = ($"{nokori}日");
+            }
+            else if(i == 2)
+            {
+                label2.Text = ($"{nokori_minutes}分");
+            }
+            else if (i == 3)
+            {
+                label2.Text = ($"{nokori_second}秒");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i= 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            i= 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            i= 3;
+        }
+    }
+}
